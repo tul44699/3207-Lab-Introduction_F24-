85a00849fa8dade7098fcf507e73324dde453320
Maxwell Ihnat <tul44699@temple.edu>
2024-08-30 15:32:11 -0400

    fix print_random incorrect code

--- print_random.c
+++ print_random.c
@@ -1,8 +1,18 @@
-// Given Project code
-char *rand_string_alloc(size_t size) {
-  char *s = malloc(size + 1);
-  if (s) {
-    rand_string(s, size);
-  }
-  return s;
+#include "random.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+  int a;
+
+  /* seed the randomizer */
+  srand((unsigned)time(NULL));
+
+  printf("Today's random word: ");
+  for (a = 0; a < 7; a++)
+    putchar(randchar());
+  putchar('\n');
+
+  return (0);
 }
--- random.c
+++ random.c
@@ -4,15 +4,3 @@
 #include <time.h>
 
 char randchar() { return 'a' + (rand() % 26); }
-
-int main() {
-  srand(time(NULL));
-
-  for (size_t i = 0; i < 7; i++) {
-    printf("%c", randchar());
-  }
-
-  printf("\n");
-
-  return 0;
-}
